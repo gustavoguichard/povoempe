fc318959e997f1bd5293f8262bbad32273a98108
Gustavo Guichard <gustavoguichard@gmail.com>
2023-01-25 13:10:40 -0300

    Add max-scale and max-width for images to avoid breaking the layout on mobile

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,7 @@ import styles from '~/styles.css'
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'Povo em Pé',
-  viewport: 'width=device-width,initial-scale=1',
+  viewport: 'width=device-width,initial-scale=1,maximum-scale=1',
 })
 
 export const links: LinksFunction = () => [
